44abf300ec331943013a5af179f852b76651cce1
KevinCalcin <kevinortegacalcin@gmail.com>
2021-09-25 22:22:27 -0500

    cambio

--- src/main/java/pe/dogwalker/model/repository/DuenoRepository.java
+++ src/main/java/pe/dogwalker/model/repository/DuenoRepository.java
@@ -15,37 +15,37 @@ import pe.dogwalker.model.entity.Dueno;
 public class DuenoRepository implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	@PersistenceContext(unitName="pdw")
+
+	@PersistenceContext(unitName = "pdw")
 	private EntityManager em;
-	
+
 	public Long insert(Dueno dueno) throws Exception {
 		em.persist(dueno);
 		return dueno.getId();
 	}
-	
+
 	public Long update(Dueno dueno) throws Exception {
 		em.merge(dueno);
 		return dueno.getId();
 	}
-	
-	public void delete (Dueno dueno) throws Exception {
-		em.remove(em.contains(dueno)?dueno: em.merge(dueno));
+
+	public void delete(Dueno dueno) throws Exception {
+		em.remove(em.contains(dueno) ? dueno : em.merge(dueno));
 	}
-	
+
 	public List<Dueno> findAll() throws Exception {
-		List<Dueno> lista = new ArrayList<>(); 
+		List<Dueno> lista = new ArrayList<>();
 		TypedQuery<Dueno> query = em.createQuery("SELECT p FROM Dueno p", Dueno.class);
 		lista = query.getResultList();
-		return lista;		
+		return lista;
 	}
-	
+
 	public List<Dueno> findByName(String name) throws Exception {
-		List<Dueno> lista = new ArrayList<>(); 
+		List<Dueno> lista = new ArrayList<>();
 		TypedQuery<Dueno> query = em.createQuery("SELECT p FROM Dueno p WHERE p.nombre LIKE ?1", Dueno.class);
 		query.setParameter(1, "%" + name + "%");
 		lista = query.getResultList();
-		return lista;		
-	}	
+		return lista;
+	}
 
 }
